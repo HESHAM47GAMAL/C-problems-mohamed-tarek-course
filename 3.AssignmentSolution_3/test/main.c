1db459cbf0893d355dc5eb42dd6c708cc41790c1
HESHAM47GAMAL <hesham.shehatah00@eng-st.cu.edu.eg>
2023-12-13 09:32:58 +0200

    remaining file

--- 3.AssignmentSolution_3/test/main.c
+++ 3.AssignmentSolution_3/test/main.c
@@ -1,34 +1,25 @@
 #include <stdio.h>
 
-
-void InsertionSort( int * const  , short);
-
-int main() {
-
-    int R[]= {2 , 7 , 4 , 1 , 5 ,3};
-    short sizeR = sizeof(R) / sizeof(R[0]);
-    InsertionSort(R ,sizeR );
-    printf ( "Sorted array by Selection : ");
-    for(short i =0 ; i< sizeR ; i++)
-        printf("%d ",R[i]);
-
-    return 0;
-}
-
-void InsertionSort(int *const T, short size_R)
+struct date
 {
-    for(short outer = 0 ; outer < size_R - 1 ; outer ++)
-    {
-        short MinIndex = outer ;
-        for(short inner = outer + 1 ;  inner < size_R ; inner ++)
-        {
-            if(T[MinIndex] > T[inner])
-                MinIndex = inner;
-        }
-        int temp = T[outer];
-        T[outer] = T[MinIndex];
-        T[MinIndex] = temp;
-    }
+	int day ;
+	int month;
+	int year;
+};
 
+struct employee
+{
+	int age;
+	int salary ;
+	struct date D;
+};
+
+int main()
+{
+
+	struct employee E1={23,15000,{27,9,2000}} , * ptr_struct =&E1 ;
+//	ptr_struct = &E1;
+	printf("age = %d",(*ptr_struct).D.day);
+
+	return 0;
 }
-
